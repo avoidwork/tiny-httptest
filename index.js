d6770468718abab6b71be3b6577626a7b35a77ee
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-02-18 15:52:41 -0500

    Fleshing out factory

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const http = require("http"),
 	path = require("path"),
 	TinyHTTPTest = require(path.join(__dirname, "lib", "tinyhttptest"));
 
-function factory ({url, method = "get", body = null, headers = {}} = {url: "http://localhost", method: "get", body: null, headers: {}}) {
+function factory ({url, method = "get", body = null, headers = {}, timeout = 30000} = {url: "http://localhost", method: "get", body: null, headers: {}, timeout: 30000}) {
 	let obj;
 
 	if (http.METHODS.includes(method.toUpperCase())) {
-		 obj = new TinyHTTPTest(url, method, body, headers);
+		 obj = new TinyHTTPTest(url, method, body, headers, timeout);
 	} else {
 		throw new Error("Invalid HTTP method");
 	}
 
 	return obj;
 }
-let x = factory();
+
 module.exports = factory;
