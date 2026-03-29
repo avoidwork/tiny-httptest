e60127e5a2ec5f4d3af2e39c0d7e43ac13cf2925
Jason Mulligan <jason.mulligan@avoidwork.com>
2026-03-28 22:35:36 -0400

    Update TypeScript definitions with proper types

--- types/httptest.d.ts
+++ types/httptest.d.ts
@@ -1,47 +1,60 @@
-export function httptest({ url, method, body, headers, timeout }?: {
-    url?: string;
-    method?: string;
-    body?: any;
-    headers?: {};
-    timeout?: number;
-}): HTTPTest;
-export class HTTPTest {
-    constructor(uri: any, method: any, headers: any, body: any, timeout: any);
-    body: string;
-    capture: any;
-    etag: boolean;
-    expects: any;
-    headers: {};
-    options: {
-        body: any;
-        hostname: any;
-        method: any;
-        path: string;
-        port: any;
-        protocol: any;
-        headers: any;
-        timeout: any;
-    };
-    jar: boolean;
-    req: any;
-    res: any;
-    reuse: any;
-    status: number;
-    captureHeader(name: any): this;
-    cookies(state?: boolean): this;
-    cors(arg: any, success?: boolean): this;
-    end(): any;
-    etags(state?: boolean): this;
-    expectBody(value?: RegExp): this;
-    expectHeader(name: any, value?: RegExp): this;
-    expectJson(): this;
-    expectStatus(value?: number): this;
-    expectValue(name: any, value: any): this;
-    json(arg?: any): this;
-    process(): this;
-    request(): any;
-    reuseHeader(name: any): this;
-    send(arg: any): this;
-    test(arg: any, value: any, err: any): this;
-    warning(type: any, a: any, b: any, k: any): string;
+export type TestBody = string | object | Array<any>;
+
+export type ExpectationValue = RegExp | Function | string | number;
+
+export interface TestOptions {
+	url?: string;
+	method?: string;
+	body?: TestBody;
+	headers?: Record<string, any>;
+	timeout?: number;
 }
+
+export class HTTPTest {
+	constructor(uri: string, method: string, headers: Record<string, any>, body: TestBody, timeout: number);
+
+	options: {
+		body: TestBody;
+		hostname: string;
+		method: string;
+		path: string;
+		port: string | number;
+		protocol: string;
+		headers: Record<string, any>;
+		timeout: number;
+	};
+
+	captureHeader(name: string): this;
+
+	cookies(state?: boolean): this;
+
+	cors(arg?: string, success?: boolean): this;
+
+	end(): Promise<this>;
+
+	etags(state?: boolean): this;
+
+	expectBody(value?: ExpectationValue): this;
+
+	expectHeader(name: string, value?: ExpectationValue): this;
+
+	expectJson(): this;
+
+	expectStatus(value?: number): this;
+
+	expectValue(name: string, value: any): this;
+
+	json(arg?: TestBody): this;
+
+	process(): this;
+
+	reuseHeader(name: string): this;
+
+	send(arg: TestBody): this;
+
+	test(arg: any, value: any, err: string): this;
+
+	warning(type: string, a: any, b: any, k?: string): string;
+}
+
+export function httptest(options?: TestOptions): HTTPTest;
